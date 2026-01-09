c8f86576718697d028cc6bb9366920d2f2e283aa
Captainzik <j.sparrowworld@gmail.com>
2026-01-08 18:37:17 -0600

    beginning of file

---
+++ tailwind.config.ts
@@ -0,0 +1,24 @@
+import type { Config } from 'tailwindcss'
+
+const config: Config = {
+  content: [
+    './app/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+    // add any other paths
+  ],
+  theme: {
+    extend: {
+      colors: {
+        border: 'hsl(var(--border))',
+        input: 'hsl(var(--input))',
+        ring: 'hsl(var(--ring))',
+        background: 'hsl(var(--background))',
+        foreground: 'hsl(var(--foreground))',
+        // ... other custom colors if needed
+      },
+    },
+  },
+  plugins: [],
+}
+
+export default config
